332eb5304279b429b157a39c489641400c2d3980
Eshemiedomi Samuel Oghenevwede <53244114+vwedesam@users.noreply.github.com>
2022-05-20 22:23:06 +0100

    add fields test

--- automation/test.py
+++ automation/test.py
@@ -7,5 +7,12 @@ url = "https://www.mni.net/contact/"
 form = AutomateForm(PATH, url)
 
 
+form.find_and_set_field('email', 'crypticologist@gmail.com', '_email')
+
+form.find_and_set_field('fullname', 'wisdom Oga', 'full_name')
+
+form.find_and_set_field('firstname', 'wisdom', 'first_name')
+
+form.find_and_set_field('lastname', 'Oga', 'last_name')
 
 
